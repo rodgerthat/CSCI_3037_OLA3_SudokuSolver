685cb9902b752ffdc0359e9af06ecdddf1ba94da
joelrnorris <joel.r.norris@gmail.com>
2018-03-12 18:14:43 -0500

    finished-ish solver

--- SudokuSolver/Solver.cs
+++ SudokuSolver/Solver.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// A Backtracking program  in C++ to solve Sudoku problem
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,31 +9,45 @@ namespace SudokuSolver
 {
     class Solver
     {
-        // A Backtracking program  in C++ to solve Sudoku problem
-        #include <stdio.h>
+        
+        // default sudoku grid
+        // 0 means unassigned cells
+        int[,] grid = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+                      {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                      {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                      {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                      {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                      {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                      {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                      {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        };
+
 
         // UNASSIGNED is used for empty cells in sudoku grid
-        #define UNASSIGNED 0
+        private const int UNASSIGNED = 0;
 
         // N is used for size of Sudoku grid. Size will be NxN
-        #define N 9
+        private const int N = 9;
 
         // This function finds an entry in grid that is still unassigned
-        bool FindUnassignedLocation(int grid[N][N], int &row, int &col);
+        // private bool FindUnassignedLocation(int[,] grid, int row, int col);
 
         // Checks whether it will be legal to assign num to the given row,col
-        bool isSafe(int grid[N][N], int row, int col, int num);
+        // private bool isSafe(int[,] grid, int row, int col, int num);
 
         /* Takes a partially filled-in grid and attempts to assign values to
           all unassigned locations in such a way to meet the requirements
           for Sudoku solution (non-duplication across rows, columns, and boxes) */
-        bool SolveSudoku(int grid[N][N])
+        bool SolveSudoku(int[,] grid)
         {
             int row, col;
 
             // If there is no unassigned location, we are done
             if (!FindUnassignedLocation(grid, row, col))
-               return true; // success!
+            {
+                return true; // success!
+            }
 
             // consider digits 1 to 9
             for (int num = 1; num <= 9; num++)
@@ -41,18 +56,86 @@ namespace SudokuSolver
                 if (isSafe(grid, row, col, num))
                 {
                     // make tentative assignment
-                    grid[row][col] = num;
+                    grid[row, col] = num;
 
                     // return, if success, yay!
                     if (SolveSudoku(grid))
                         return true;
 
                     // failure, unmake & try again
-                    grid[row][col] = UNASSIGNED;
-                }
-                                                                                                                               1,1           Top
+                    grid[row, col] = UNASSIGNED;
+                } 
+            }
+            return false; // this triggers backtracking
+        }
 
-                                                                                                                                   132,1         Bot
+        /* Searches the grid to find an entry that is still unassigned. If
+        found, the reference parameters row, col will be set the location
+        that is unassigned, and true is returned. If no unassigned entries
+        remain, false is returned. */
+        private bool FindUnassignedLocation(int[,] grid, int row, int col)
+        {
+            for (row = 0; row < N; row++)
+                for (col = 0; col < N; col++)
+                    if (grid[row, col] == UNASSIGNED)
+                        return true;
+            return false;
+        }
 
+        /* Returns a boolean which indicates whether any assigned entry
+           in the specified row matches the given number. */
+        bool UsedInRow(int[,] grid, int row, int num)
+        {
+            for (int col = 0; col < N; col++)
+                if (grid[row, col] == num)
+                    return true;
+            return false;
+        }
+
+        /* Returns a boolean which indicates whether any assigned entry
+        in the specified column matches the given number. */
+        bool UsedInCol(int[,] grid, int col, int num)
+        {
+            for (int row = 0; row < N; row++)
+                if (grid[row, col] == num)
+                    return true;
+            return false;
+        }
+
+        /* Returns a boolean which indicates whether any assigned entry
+           within the specified 3x3 box matches the given number. */
+        bool UsedInBox(int[,] grid, int boxStartRow, int boxStartCol, int num)
+        {
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
+                    if (grid[row+boxStartRow, col+boxStartCol] == num)
+                        return true;
+            return false;
+        }
+
+        /* Returns a boolean which indicates whether it will be legal to assign
+           num to the given row,col location. */
+        private bool isSafe(int[,] grid, int row, int col, int num)
+        {
+            /* Check if 'num' is not already placed in current row,
+               current column and current 3x3 box */
+            return !UsedInRow(grid, row, num) &&
+                   !UsedInCol(grid, col, num) &&
+                   !UsedInBox(grid, row - row%3 , col - col%3, num);
+        }
+
+        /* A utility function to print grid  */
+        /*
+        void printGrid(int[,] grid)
+        {
+            for (int row = 0; row < N; row++)
+            {
+               for (int col = 0; col < N; col++)
+                     printf("%2d", grid[row][col]);
+                printf("\n");
+            }
+        }
+        */
+                                                                                                       1,1           Top
     }
 }
